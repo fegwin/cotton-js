691142e34481648f942c34413e0e92db86d2b1c1
Aswin Lakshman <shwin.lakes@gmail.com>
2018-04-11 00:08:01 +1000

    Ok 😒. No fancy fancy.

--- util/index.js
+++ util/index.js
@@ -2,13 +2,11 @@ import image from './image';
 import json from './json';
 import { BoundingBox, getRandomNumber, Matrix, Vec } from './math';
 
-const toExport = {
-  ...image,
-  ...json,
+const toExport = Object.assign(image, json, {
   BoundingBox,
   getRandomNumber,
   Matrix,
   Vec,
-};
+});
 
 export default toExport;
