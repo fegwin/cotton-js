43af08dab7922e616f159a318c87f9504fa3c157
Aswin Lakshman <shwin.lakes@gmail.com>
2018-04-10 22:59:52 +1000

    Added eslintrc file. Fixed missing exports.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import * as _util from './util';
+import Layer from './layer';
+import Entity from './entity';
+import Animator from './animator';
+import Compositor from './compositor';
 
-export const util = {..._util};
+export const util = { ..._util };
 export { default as Layer } from './layer';
 export { default as Entity } from './entity';
 export { default as Animator } from './animator';
+export { default as Compositor } from './compositor';
+
+export default {
+  Util: _util,
+  Layer,
+  Entity,
+  Animator,
+  Compositor,
+};
