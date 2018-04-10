634a378a3e6ed5236de04b8aa2951e8c0757fb0d
Aswin Lakshman <shwin.lakes@gmail.com>
2018-04-11 00:33:48 +1000

    Moved some stuff around. Added a build step. Js too edgy before...

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'cotton.js',
+  },
+  // module: {
+  //   loaders: [
+  //     {
+  //       test: /\.js$/,
+  //       loader: 'babel-loader',
+  //       query: {
+  //         presets: ['es2015'],
+  //       },
+  //     },
+  //   ],
+  // },
+};
