088668be8ca87b1203d0dac576c40b271a0045f7
Aswin Lakshman <shwin.lakes@gmail.com>
2018-04-13 00:48:13 +1000

    Removed weird exports

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import Entity from "./entity";
 import Animator from "./animator";
 import Compositor from "./compositor";
 
-export { default as util } from "./util";
-export { default as Layer } from "./layer";
-export { default as Entity } from "./entity";
-export { default as Animator } from "./animator";
-export { default as Compositor } from "./compositor";
+// export { default as util } from "./util";
+// export { default as Layer } from "./layer";
+// export { default as Entity } from "./entity";
+// export { default as Animator } from "./animator";
+// export { default as Compositor } from "./compositor";
 
-export default {
+export {
   util,
   Layer,
   Entity,
