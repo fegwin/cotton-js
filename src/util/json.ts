ee96ca47e6e81d11143230e7887913dd7dd71abc
kyrimsteele <kyrimcsteele@gmail.com>
2018-04-19 22:07:28 +1000

    Forgot these files

--- src/util/json.ts
+++ src/util/json.ts
@@ -1,5 +1,5 @@
 export class Json {
-  loadJson(url: string): Promise<Response> {
-    return fetch(url).then(r => r.json());
+  public loadJson(url: string): Promise<Response> {
+    return fetch(url).then((r) => r.json());
   }
 }
