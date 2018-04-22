02e418af937ee29d3a796a0aad14141ebd61944f
Aswin Lakshman <shwin.lakes@gmail.com>
2018-04-22 17:46:03 +1000

    Added export for EntityGraph

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Layer } from "./layer";
 export { Entity } from "./entity";
 export { Animator } from "./animator";
 export { Compositor } from "./compositor";
+export { EntityGraph } from "./entity-graph";
