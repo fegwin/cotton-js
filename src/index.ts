494c538ebacaae7acfc4252b4f2cbb5e5276ab16
Aswin Lakshman <shwin.lakes@gmail.com>
2018-05-13 13:45:11 +1000

    Spritesheet implo (#28)
    * Added class SpriteSheet
    
    * Some sprities bruz
    
    * Added basic spritesheet support
    
    * More sprite stuff
    
    * Added some documentation
    
    * Added a full stop. -__-
    
    * More...
    
    * Updated SpriteSheet to work with MemoryCanvas
    
    * Implemented interfaces for sprite defs

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export { Entity } from "./entity";
 export { Animator } from "./animator";
 export { Compositor } from "./compositor";
 export { EntityLibrary } from "./entity-library";
+export { SpriteSheet, IAnimation, ISprite, ISpriteDefinition } from "./sprite-sheet";
 export { Trait } from "./trait";
 export { traits } from "./traits";
--- src/util/image.ts
+++ src/util/image.ts
@@ -1,13 +1,11 @@
 import { Promise } from "es6-promise";
 
-export class CottonImage {
-  public loadImage(url: string): Promise<HTMLImageElement> {
-    return new Promise((resolve) => {
-      const img = new Image();
-      img.addEventListener("load", () => {
-        resolve(img);
-      });
-      img.src = url;
+export function loadImage(url: string): Promise<HTMLImageElement> {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.addEventListener("load", () => {
+      resolve(img);
     });
-  }
+    img.src = url;
+  });
 }
